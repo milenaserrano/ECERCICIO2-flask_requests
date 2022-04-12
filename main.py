10b6ef918ca0e0855e72864efb59777c35a89968
Milena <68025219+milenaserrano@users.noreply.github.com>
2022-04-12 13:50:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect
+app = Flask('app')
+
+contacts = []
+
+@app.route('/')
+def index():
+  return render_template(
+    'index.html',
+    contacts=contacts
+  )
+
+@app.route('/create', methods=['POST'])
+def create():
+  name = request.form.get('name')
+  email = request.form.get('email')
+  phone = request.form.get('phone')
+  contacts.append({
+    'name': name,
+    'email': email,
+    'phone': phone,
+  })
+  return redirect('/')
+
+
+@app.route('/update/<int:index>')
+def update(index):
+  name = request.form.get('name')
+  email = request.form.get('email')  
+  phone = request.form.get('phone')
+  contacts[index]['name'] = name
+  contacts[index]['email'] = email
+  contacts[index]['phone'] = phone
+  return redirect('/')
+
+@app.route('/delete/<int:index>')
+def delete(index):
+  name = request.form.get('name')
+  email = request.form.get('email')  
+  phone = request.form.get('phone')
+  contacts[index]['name'] = name
+  contacts[index]['email'] = email
+  contacts[index]['phone'] = phone
+  return redirect('/')
+  
+# IMPORTANTE V
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
