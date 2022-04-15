258962667e6bd8a1d76853e9d695f50365f3c34e
Milena <68025219+milenaserrano@users.noreply.github.com>
2022-04-15 00:02:58 +0000

    EXERCICIO OK

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+#TERMINEI: 14/04/2022
+# COPIEI DO TODO NO DB DO PROF EDUARDO (ednascimento) e deu certo finalmente! 
+
+# Você foi encarregado de criar um sistema de agenda de contatos. Primeiramente crie uma variavel chamada contacts para armazenar os contatos da agenda
+
+#Logo após, crie a rota principal (/) com a frase Agenda de Contatos em h1, que mostre uma lista de contatos cadastrados para serem editados e deletados e tenha um fomulário com três inputs de nomes name, email e phone. Faça as rotas (/create) que recebe os dados do formulário via POST e cadastre um contato, (/delete) que deleta um contato específico e (/update) que edita um contato específico. Utilize a imagem a seguir como referência.
+
+
 from flask import Flask, render_template, request, redirect
 app = Flask('app')
 
